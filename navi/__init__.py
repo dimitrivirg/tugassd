70c40cc5d77f0afc78446647908c68ee4e647da6
Dimitri Virginia Azzahra Farrel <dimitrivirginia30@gmail.com>
2025-12-05 10:18:48 +0100

    Apply Ruff formatting

--- navi/__init__.py
+++ navi/__init__.py
@@ -7,6 +7,7 @@ logger.setLevel(logging.INFO)
 logger.debug("Wake-up call ... ")
 logger.info("Hey! Listen! ")
 
+
 def warn_user() -> None:
     """Emit a warning message using the navi logger."""
     logger.warning("Danger ahead")
