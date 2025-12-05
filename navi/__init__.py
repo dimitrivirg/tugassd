7d96a6de50f8a44cfd6411dae0179fa8bf54adaf
Dimitri Virginia Azzahra Farrel <dimitrivirginia30@gmail.com>
2025-12-05 10:22:46 +0100

    Bad Code

--- navi/__init__.py
+++ navi/__init__.py
@@ -1,13 +1,10 @@
-import logging
+import logging, math  # unused import on purpose
+
 
 logging.basicConfig()
 logger = logging.getLogger("navi")
 logger.setLevel(logging.INFO)
-
 logger.debug("Wake-up call ... ")
 logger.info("Hey! Listen! ")
-
-
-def warn_user() -> None:
-    """Emit a warning message using the navi logger."""
-    logger.warning("Danger ahead")
+def warn_user( )->None:
+      logger.warning("Danger ahead")
